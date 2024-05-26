063976c16cda036e7b0181900f0c3d5c7ea379ac
JuanP10 <ramirezgutierrez32@gmail.com>
2024-05-25 23:38:45 -0500

    Añadido Servicio Booking

---
+++ CarInventoryService/src/main/java/org/example/carinventoryservice/Models/Mappers/CarMapper.java
@@ -0,0 +1,11 @@
+package org.example.carinventoryservice.Models.Mappers;
+
+import org.example.carinventoryservice.Models.Dtos.CarDtoSave;
+import org.example.carinventoryservice.Models.Dtos.CarDtoSend;
+import org.example.carinventoryservice.Models.Entities.CarInventory;
+import
+
+
+public class CarMapper {
+
+}
