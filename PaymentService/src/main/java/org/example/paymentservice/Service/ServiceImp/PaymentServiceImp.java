242541205d1ea047e9958b5d4e9863cc65674570
JuanP10 <ramirezgutierrez32@gmail.com>
2024-05-27 20:04:26 -0500

    Añadido de DockerCompose

--- PaymentService/src/main/java/org/example/paymentservice/Service/ServiceImp/PaymentServiceImp.java
+++ PaymentService/src/main/java/org/example/paymentservice/Service/ServiceImp/PaymentServiceImp.java
@@ -14,10 +14,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -37,47 +37,7 @@ public class PaymentServiceImp implements PaymentService{
 
     @Override
     public PaymentDtoSend guardar(PaymentDtoSave paymentDtoSave) {
-        String urlBase = "http://container3-app:8080/microservice/booking";
-        String urlBooking = urlBase + "/" + paymentDtoSave.getIdBooking();
-        String urlBookingCompletar = urlBase + "/completar/" + paymentDtoSave.getIdBooking();
-        String urlBookingFail = urlBase + "/failed/" + paymentDtoSave.getIdBooking();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
-
-        try {
-            ResponseEntity<BookingRequest> bookingResponseEntity =
-                    restTemplate.getForEntity(urlBooking, BookingRequest.class);
-            BookingRequest bookingRequest = Objects.requireNonNull(bookingResponseEntity.getBody());
-
-            Payment payment = paymentMapper.toEntity(paymentDtoSave);
-
-            if (bookingRequest.getStatus().equals(BookingRequest.Status.CONFIRMED.name()) ||
-                    bookingRequest.getStatus().equals(BookingRequest.Status.FAILED.name())) {
-                payment.setStatus(Payment.PaymentEstatus.COMPLETED);
-                payment.setTransactionId(UUID.randomUUID());
-
-                restTemplate.exchange(urlBookingCompletar, HttpMethod.PUT, requestEntity, String.class);
-                return paymentMapper.toDtoSend(paymentRepository.save(payment));
-            }
-            payment.setStatus(Payment.PaymentEstatus.FAILED);
-            PaymentDtoSend paymentDtoSend = paymentMapper.toDtoSend(paymentRepository.save(payment));
-
-            if (!bookingRequest.getStatus().equals(BookingRequest.Status.COMPLETE.name())) {
-                restTemplate.exchange(urlBookingFail, HttpMethod.PUT, requestEntity, String.class);
-            }
-            throw new RuntimeException("Fallo al procesar el pago");
-        } catch (HttpClientErrorException ex) {
-
-            throw new RuntimeException("Error HTTP al procesar el pago: " + ex.getStatusCode());
-        } catch (RestClientException ex) {
-
-            throw new RuntimeException("Error al conectarse al servidor: " + ex.getMessage());
-        } catch (Exception ex) {
-
-            throw new RuntimeException("Error inesperado al procesar el pago: " + ex.getMessage());
-        }
+        return null;
     }
 
 
